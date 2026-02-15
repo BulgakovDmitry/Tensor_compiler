e95d4481787a4c9574877adf00d6c9c452cb7110
RTCupid <19apr2006@vk.com>
2026-02-15 21:18:13 +0300

    [driver.hpp] add driver

---
+++ include/driver.hpp
@@ -0,0 +1,28 @@
+#ifndef INCLUDE_DRIVER_HPP
+#define INCLUDE_DRIVER_HPP
+
+#include <fstream>
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include "onnx/onnx_pb.h"
+
+namespace blab {
+
+inline int driver() {
+    onnx::ModelProto model;
+    std::fstream input("mnist-12.onnx", std::ios::in | std::ios::binary);
+
+    if (!model.ParseFromIstream(&input)) {
+        std::cerr << "Failed to parse ONNX model." << '\n';
+        return -1;
+    }
+
+    const auto& graph = model.graph();
+    std::cout << "Graph '" << graph.name() << "' loaded." << '\n';
+    std::cout << "Number of nodes: " << graph.node_size() << '\n';
+
+    return 0;
+}
+
+} // namespace blab
+
+#endif // INCLUDE_DRIVER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,7 @@
-#include "onnx/onnx_pb.h"
-#include <fstream>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include "driver.hpp"
 
 int main() {
-  onnx::ModelProto model;
-  std::fstream input("mnist-12.onnx", std::ios::in | std::ios::binary);
-
-  if (!model.ParseFromIstream(&input)) {
-    std::cerr << "Failed to parse ONNX model." << std::endl;
-    return -1;
-  }
-
-  const auto &graph = model.graph();
-  std::cout << "Graph '" << graph.name() << "' loaded." << std::endl;
-  std::cout << "Number of nodes: " << graph.node_size() << std::endl;
+    blab::driver();
 
   return 0;
 }
