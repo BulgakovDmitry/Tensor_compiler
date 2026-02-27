1b0d150b4c902e6a713ecbd4b356900f643ef861
RTCupid <19apr2006@vk.com>
2026-02-27 19:21:53 +0300

    [add documentation for data structure]

--- include/structure/node.hpp
+++ include/structure/node.hpp
@@ -15,6 +15,12 @@ namespace tensor_compiler {
 
 using Attributes = std::unordered_map<std::string, Attribute>;
 
+/**
+ * @brief Represents a node in the computation graph.
+ *
+ * A node corresponds to an operator in the neural network. It stores the * operator type (opcode), its inputs/outputs as tensor names, and a set of
+ * attributes that parameterize the operator.
+ */
 class Node {
   public:
     using node_id = std::size_t;
@@ -31,30 +37,103 @@ class Node {
     Attributes attributes_;
 
   public:
+    /**
+     * @brief Construct a new Node object.
+     * @param name Human-readable name of the node.
+     * @param opcode Operator type (e.g., "Conv", "Relu").
+     * @param id Unique identifier (default 0).
+     */
     Node(const std::string &name, const std::string &opcode, node_id id = 0)
         : id_{id}, name_{name}, opcode_{opcode} {}
 
+    /**
+     * @brief Set the node's name.
+     * @param name New name.
+     */
     void set_name(const std::string &name);
 
+    /**
+     * @brief Get the node's unique identifier.
+     * @return node_id.
+     */
     node_id get_id() const;
+
+    /**
+     * @brief Get the operator type.
+     * @return const reference to opcode string.
+     */
     const std::string &get_opcode() const;
+
+    /**
+     * @brief Get the node's name.
+     * @return const reference to name string.
+     */
     const std::string &get_name() const;
 
+    /**
+     * @brief Get the list of input tensor names.
+     * @return const reference to vector of strings.
+     */
     const std::vector<std::string> &get_inputs() const;
+
+    /**
+     * @brief Get the list of output tensor names.
+     * @return const reference to vector of strings.
+     */
     const std::vector<std::string> &get_outputs() const;
+
+    /**
+     * @brief Get the node's attributes map.
+     * @return const reference to Attributes map.
+     */
     const Attributes &get_attributes() const;
 
+    /**
+     * @brief Set the inputs from a vector of strings.
+     * @param inputs Vector of input names.
+     */
     void set_inputs(const std::vector<std::string> &inputs);
+
+    /**
+     * @brief Set the inputs from a protobuf repeated field.
+     * @param inputs Protobuf repeated field of strings.
+     */
     void set_inputs(const name_t &inputs);
 
+    /**
+     * @brief Set the outputs from a vector of strings.
+     * @param outputs Vector of output names.
+     */
     void set_outputs(const std::vector<std::string> &outputs);
+
+    /**
+     * @brief Set the outputs from a protobuf repeated field.
+     * @param outputs Protobuf repeated field of strings.
+     */
     void set_outputs(const name_t &outputs);
 
+    /**
+     * @brief Parse attributes from an ONNX NodeProto.
+     *
+     * Reads the attribute field of the protobuf and stores them as Attribute
+     * objects. Supported types: FLOAT, INT, STRING, FLOATS, INTS.
+     * @param node ONNX NodeProto message.
+     */
     void parse_attributes(const onnx::NodeProto &node);
 
+    /**
+     * @brief Set an attribute value.
+     * @param name Attribute name.
+     * @param value Attribute value (variant type).
+     */
     void set_attribute(const std::string &name,
                        const Attribute::AttrValue &value);
 
+    /**
+     * @brief Check if an attribute exists.
+     * @param name Attribute name.
+     * @return true if present, false otherwise.
+     */
     bool has_attribute(const std::string &name) const;
 
   private:
--- include/structure/tensor.hpp
+++ include/structure/tensor.hpp
@@ -8,16 +8,23 @@
 namespace tensor_compiler {
 
 enum class Tensor_kind {
-    unknown = 0,
-    input,
-    output,
-    intermediate,
-    constant,
+    unknown = 0,      ///< Role not yet assigned.
+    input,            ///< Graph input tensor.
+    output,           ///< Graph output tensor.
+    intermediate,     ///< Intermediate tensor (result of some node).
+    constant,         ///< Constant tensor (initializer).
 };
 
 using data_type = onnx::TensorProto_DataType;
 using dim_type = google::protobuf::RepeatedField<int64_t>;
 
+/**
+ * @brief Represents a tensor in the computation graph.
+ *
+ * Stores tensor metadata: name, data type, shape, raw data, and its kind.
+ * The raw data is stored as a string (binary blob). For float tensors,
+ * a convenience factory method Tensor::create() is provided.
+ */
 class Tensor {
   private:
     std::string name_;
@@ -30,29 +37,110 @@ class Tensor {
 
   public:
     Tensor() = default;
+
+    /**
+     * @brief Construct a new Tensor object.
+     * @param name Tensor name.
+     * @param type Data type (onnx::TensorProto_DataType).
+     * @param shape Vector of dimensions.
+     * @param data Raw data as string (binary).
+     * @param kind Tensor kind (default unknown).
+     */
     Tensor(const std::string &name, data_type type, std::vector<int64_t> shape,
            const std::string &data, Tensor_kind kind = Tensor_kind::unknown)
         : name_{name}, type_{type}, kind_{kind}, data_{data}, shape_{shape} {}
 
+    /**
+     * @brief Create a float tensor from a vector of floats.
+     *
+     * Convenience method that packs the float data into a binary string.
+     * @param name Tensor name.
+     * @param shape Tensor shape.
+     * @param data Vector of float values.
+     * @param kind Tensor kind.
+     * @return Tensor object.
+     */
     static Tensor create(const std::string &name,
                          const std::vector<int64_t> &shape,
                          const std::vector<float> &data,
                          const Tensor_kind &kind);
 
+    /**
+     * @brief Get the tensor name.
+     * @return const reference to name string.
+     */
     const std::string &get_name() const;
+
+    /**
+     * @brief Get the data type.
+     * @return int (onnx::TensorProto_DataType value).
+     */
     const int get_type() const;
+
+    /**
+     * @brief Get the tensor kind.
+     * @return Tensor_kind.
+     */
     Tensor_kind get_kind() const;
+
+    /**
+     * @brief Get the raw data as a string.
+     * @return const reference to data string.
+     */
     const std::string &get_data() const;
+
+    /**
+     * @brief Get the tensor shape.
+     * @return const reference to vector of dimensions.
+     */
     const std::vector<int64_t> &get_shape() const;
+
+    /**
+     * @brief Get the protobuf dim field (may be unused).
+     * @return const dim_type.
+     */
     const dim_type get_dim() const;
 
+    /**
+     * @brief Set the tensor name.
+     * @param name New name.
+     */
     void set_name(const std::string &name);
+
+    /**
+     * @brief Set the data type.
+     * @param type onnx::TensorProto_DataType value.
+     */
     void set_type(const int type);
+
+    /**
+     * @brief Set the tensor kind.
+     * @param kind Tensor_kind.
+     */
     void set_kind(Tensor_kind kind);
+
+    /**
+     * @brief Set the raw data.
+     * @param data Binary string.
+     */
     void set_data(const std::string &data);
+
+    /**
+     * @brief Set the tensor shape.
+     * @param shape Vector of dimensions.
+     */
     void set_shape(const std::vector<int64_t> &shape);
+
+    /**
+     * @brief Set the protobuf dim field.
+     * @param dim dim_type.
+     */
     void set_dim(const dim_type dim);
 
+    /**
+     * @brief Check if the tensor is a constant (initializer).
+     * @return true if kind_ == Tensor_kind::constant.
+     */
     bool is_constant() const;
 };
 
